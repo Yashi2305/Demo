3a87cfb52576dc7fa066e5b0a9e70dd9e52c0194
Yashi2305 <singhyashi.800@gmail.com>
2024-08-31 12:40:08 +0530

    Add new files or a brief description of changes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,32 +1,56 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png';
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 import { FaSearch } from "react-icons/fa";
 import { SlBasket } from "react-icons/sl";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
 
-  const [menu,setMenu]=useState("home");
   return (
-    <div className='navbar'>
-        <img src={logo} alt='' className='logo'/>
-      <ul className='navbar-menu'>
-        <Link to='/' onClick={()=>setMenu("home")} className={menu==='home'?"active":""}>Home</Link>
-        <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==='menu'?"active":""}>Menu</a> 
-        <a href="#app-download" onClick={()=>setMenu("mobile-app")} className={menu==='mobile-app'?"active":""}>Mobile App</a>
-        <a href="#footer" onClick={()=>setMenu("contact")} className={menu==='contact'?"active":""}>Contact Us</a>
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact")}
+          className={menu === "contact" ? "active" : ""}
+        >
+          Contact Us
+        </a>
       </ul>
       <div className="navbar-right">
-      <FaSearch />
-      <div className="navbar-search-icon">
-      <SlBasket />
-      <div className="dot"></div>
-      </div>
-      <button>Sign in</button>
+        <FaSearch />
+        <div className="navbar-search-icon">
+          <SlBasket />
+          <div className="dot"></div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
